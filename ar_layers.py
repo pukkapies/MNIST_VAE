7b6c14a4a8541f0d040d7017252c6af3f08df52e
pukkapies <knwebster@me.com>
2017-05-16 22:16:37 +0100

    Make ar linear mask Python 3 friendly

--- ar_layers.py
+++ ar_layers.py
@@ -8,7 +8,7 @@ def get_linear_ar_mask(n_in, n_out, zerodiagonal=False):
 
     mask = np.ones([n_in, n_out], dtype=np.float32)
     if n_out >= n_in:
-        k = n_out / n_in
+        k = n_out // n_in
         for i in range(n_in):
             mask[i + 1:, i * k:(i + 1) * k] = 0
             if zerodiagonal:
